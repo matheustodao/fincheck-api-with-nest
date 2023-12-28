5bb7c80612595a90e059198ef8924266f52d374c
matheustodao <todaomatheus@hotmail.com>
2023-12-28 14:28:51 -0300

    refactor: change user creation in user module to auth module at register

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post, UsePipes } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthenticateDto } from './dto/authenticate.dto';
+import { RegisterUserDto } from './dto/register-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -10,5 +11,11 @@ export class AuthController {
   @UsePipes()
   authenticate(@Body() authenticateDto: AuthenticateDto) {
     return this.authService.authenticate(authenticateDto);
+  }
+
+  @Post('signup')
+  @UsePipes()
+  register(@Body() registerUserDto: RegisterUserDto) {
+    return this.authService.register(registerUserDto);
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,9 +1,15 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { compare } from 'bcryptjs';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { compare, hash } from 'bcryptjs';
 
 import { AuthenticateDto } from './dto/authenticate.dto';
 import { UsersRepositories } from 'src/shared/database/repositories/users.repositories';
 import { JwtService } from '@nestjs/jwt';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { CategoryConstants } from 'src/shared/constants/category.constants';
 
 @Injectable()
 export class AuthService {
@@ -28,5 +34,36 @@ export class AuthService {
     const accessToken = await this.jwtService.signAsync({ sub: user.id });
 
     return { ...user, accessToken };
+  }
+
+  async register({ name, email, password }: RegisterUserDto) {
+    const emailAlreadyTaken = await this.usersRepo.findByEmail(email);
+
+    if (emailAlreadyTaken) {
+      throw new ConflictException('This email is already in use');
+    }
+
+    const hashedPassword = await hash(password, 12);
+
+    const created = await this.usersRepo.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        category: {
+          createMany: {
+            data: CategoryConstants as never,
+          },
+        },
+      },
+
+      include: {
+        bankAccounts: true,
+        category: true,
+        transaction: true,
+      },
+    });
+
+    return created;
   }
 }
