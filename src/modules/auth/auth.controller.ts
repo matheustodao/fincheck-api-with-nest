3e06518b8b38c5c9b52747acce61a83923d7fca3
matheustodao <todaomatheus@hotmail.com>
2024-02-24 20:29:30 -0300

    feat: add endpoints as public

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UsePipes } from '@nestjs/common';
+import { Body, Controller, Post, SetMetadata, UsePipes } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/sign-in.dto';
 import { SignUpDto } from './dto/sign-up.dto';
@@ -8,12 +8,14 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @SetMetadata('IS_PUBLIC', true)
   @UsePipes()
   authenticate(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
   }
 
   @Post('signup')
+  @SetMetadata('IS_PUBLIC', true)
   @UsePipes()
   signUp(@Body() signUpUserDto: SignUpDto) {
     return this.authService.signUp(signUpUserDto);
