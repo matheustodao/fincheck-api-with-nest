1f461f8c5995b9d588e8c5fe31ce2fe12c09e4fe
matheustodao <todaomatheus@hotmail.com>
2024-02-24 20:30:48 -0300

    feat: create endpoint to get user account information

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,3 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { UsersRepositories } from 'src/shared/database/repositories/users.repositories';
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly UsersRepo: UsersRepositories) {}
+
+  async getUserById(userId: string) {
+    const user = await this.UsersRepo.findById(userId);
+
+    return {
+      ...user,
+      password: undefined,
+      id: undefined,
+    };
+  }
+}
