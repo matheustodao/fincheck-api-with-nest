f44011fee2a9f292eb8c98d5ad5f4ccd91bd0acb
Matheus Todao <itodaomatheus@gmail.com>
2024-07-11 21:23:05 -0300

    refactor: add decorator isPublic instead use set metada

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,21 +1,22 @@
-import { Body, Controller, Post, SetMetadata, UsePipes } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/sign-in.dto';
 import { SignUpDto } from './dto/sign-up.dto';
+import { IsPublic } from 'src/shared/decorators/IsPublic';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  @SetMetadata('IS_PUBLIC', true)
+  @IsPublic()
   @UsePipes()
   authenticate(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
   }
 
   @Post('signup')
-  @SetMetadata('IS_PUBLIC', true)
+  @IsPublic()
   @UsePipes()
   signUp(@Body() signUpUserDto: SignUpDto) {
     return this.authService.signUp(signUpUserDto);
