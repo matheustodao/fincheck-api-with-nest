747c3d82965c408be8222e3714f9963ceb459106
Matheus Todao <itodaomatheus@gmail.com>
2024-07-11 21:21:21 -0300

    chore: add PORT to variable environment

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { env } from './shared/config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3001);
+  await app.listen(env.PORT);
 }
 bootstrap();
--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -2,6 +2,9 @@ import { plainToInstance } from 'class-transformer';
 import { IsNotEmpty, IsString, validateSync } from 'class-validator';
 
 class Env {
+  @IsNotEmpty()
+  PORT: string | number;
+
   @IsString()
   @IsNotEmpty()
   jwtSecret: string;
@@ -12,6 +15,7 @@ class Env {
 }
 
 export const env: Env = plainToInstance(Env, {
+  PORT: process.env.PORT ?? 3001,
   jwtSecret: process.env.JWT_SECRET,
   jwtExpiresIn: process.env.JWT_EXPIRES_IN,
 });
@@ -19,5 +23,6 @@ export const env: Env = plainToInstance(Env, {
 const errors = validateSync(env);
 
 if (errors.length > 0) {
+  console.log(env);
   throw new Error(JSON.stringify(errors, null, 4));
 }
